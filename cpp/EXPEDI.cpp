dca52f3be3dd31efe682638fcf8fc54b43503bb2
vvarma2020 <106948726+vvarma2020@users.noreply.github.com>
2022-10-10 20:26:07 +0530

    more problems

---
+++ cpp/EXPEDI.cpp
@@ -0,0 +1,25 @@
+// include this header or include headers you want
+// bits/stdc++.h will include all headers into the program which is unneccessary
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// An example of a "macro" you can use to write code faster.
+// #define forn(i, a, n) for (int i = a; i < n; i++)
+
+const int MAXN = 200100;
+const int MOD = 1000000007;
+
+void solve() {
+    // solve problem here
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    solve();
+
+    return 0;
+}
